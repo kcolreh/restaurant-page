a8c62e7118b27aa9c3815f1aac810fcae07ac033
Mindaugas <mindaugasgrybauskas@protonmail.com>
2023-03-09 14:21:08 +0100

    Create a basic style for the front page, including a header , section menu and the footer

--- main.js
+++ main.js
@@ -15,18 +15,21 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log('working');\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+        eval("console.log('working');\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
 
-/***/ })
+        /***/
+      })
 
-/******/ 	});
+    /******/
+  });
 /************************************************************************/
-/******/ 	
+/******/
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
-/******/ 	
-/******/ })()
-;
+  /******/
+  /******/
+})()
+  ;
