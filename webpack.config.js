9f80febda2f0d870c34c5851e5260336244cc8f9
Mindaugas <mindaugasgrybauskas@protonmail.com>
2023-03-10 14:52:39 +0100

    Delete the Html hardcore and rewrite everything in javascript

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
+	watch: true,
+	entry: {
+		index: './src/index.js',
+	},
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
@@ -17,6 +20,10 @@ module.exports = {
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: 'asset/resource',
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
 			},
 		],
 	},
